9884cf51247c5437cc49311320d896211e4db613
chenwei <chenwei10@kingsoft.com>
2020-03-02 07:57:28 +0800

    修改

--- auto-reply-server/puppeteer/AutoReply.ts
+++ auto-reply-server/puppeteer/AutoReply.ts
@@ -1,6 +1,6 @@
-import { ElementHandle, Page } from 'puppeteer'
-import { cookies } from './setCookies'
+import {ElementHandle, Page, SetCookie} from 'puppeteer'
 import Config from './Config'
+import {cookies} from './setCookies'
 
 const puppeteer = require('puppeteer')
 
@@ -11,11 +11,16 @@ async function waitingMillSeconds (ms: number) {
 }
 
 async function run () {
-	const browser = await puppeteer.launch({ headless: false })
+	const browser = await puppeteer.launch({ headless: false ,ignoreDefaultArgs: ["--enable-automation"],defaultViewport:null})
 	const page: Page = await browser.newPage()
+	await page.evaluateOnNewDocument(() => {
+		Object.defineProperty(navigator, 'webdriver', {
+			get: () => undefined,
+		});
+	})
 	await page.setCookie(...cookies)
 	await page.goto(Config.loginUrl)
-	/* 目前要手动点掉广告 */
+
 	await waitingMillSeconds(20000)
 	const element: ElementHandle = await page.waitForSelector('.already-unreply')
 	const aa = await element.$$('.chat-item')
@@ -28,10 +33,34 @@ async function run () {
 	if(textContent.trim() == '你好'){
 		console.log(textContent)
 		const textarea = await page.$('#replyTextarea')
-		await textarea.type('你好啊')
+		await textarea.type('非常好')
 		const sendBtn = await page.$('.send-btn')
 		await sendBtn.click()
 	}
+/*	let cookie = await page.evaluate(() => document.cookie);
+	let cookieList = cookie.split(';')
+	let api_uid = cookieList[0].substring(8,cookieList[0].length);
+	let _nano_fp = cookieList[1].substring(10,cookieList[1].length);*/
+
+	/*console.log("cookie :" + api_uid);
+	console.log("cookie :" + _nano_fp);
+	let api_uid1: SetCookie = {
+		name: 'api_uid',
+		value: api_uid,
+		domain: '.pinduoduo.com',
+	}
+	let nano_fp: SetCookie = {
+		name: '_nano_fp',
+		value: _nano_fp,
+		domain: 'mms.pinduoduo.com',
+	}
+	let setCookies: SetCookie[] = [api_uid1, nano_fp]
+	await page.setCookie(...setCookies)*/
+	//let newCookie =
+	/*await page.setCookie(...cookies)
+
+	/!* 目前要手动点掉广告 *!/
+	*/
 
 
 	/* 找到已回复的第一个人 */
--- auto-reply-server/puppeteer/setCookies.ts
+++ auto-reply-server/puppeteer/setCookies.ts
@@ -2,32 +2,32 @@ import { SetCookie } from 'puppeteer'
 
 const JSESSIONID: SetCookie = {
 	name: 'JSESSIONID',
-	value: 'FBC543E6D684D6DE3BEB9473A8182C26',
+	value: '71A354AF3AF57306869B44AF501390A9',
 	domain: 'mms.pinduoduo.com',
 	httpOnly: true
 }
 
 const PASS_ID: SetCookie = {
 	name: 'PASS_ID',
-	value: '1-uk3nZuXjOLmOCMJ9j+an8PHV/ynFqcFT/FKeRnsoYfx+PG7FtGY4ZGajyDE/jMtHkF/dFT+6D2KiQ1kJSE799w_541916_686302',
+	value: '1-T/HbNJRFZBZ5YaGcP73PCuiTclhpoU+KFdtjsK06Cel3U26xaUJEPWiDjl6uTRQ/fWC2yj1o/MNMqHL4gWLGaw_541916_686302',
 	domain: 'mms.pinduoduo.com',
 	httpOnly: true
 }
 
 const api_uid1: SetCookie = {
 	name: 'api_uid',
-	value: 'rBUoK13AxP4xkWez8eGUAg==',
+	value: 'rBQRlV5bR1Y7d3V9tgycAg==',
 	domain: '.pinduoduo.com',
 }
 const api_uid2: SetCookie = {
 	name: 'api_uid',
-	value: 'CiH9yV3AxP8E2AA5KsKeAg==',
+	value: 'rBUUF15bR1aSDkreX2v+Ag==',
 	domain: '.yangkeduo.com',
 }
 const nano_fp: SetCookie = {
 	name: '_nano_fp',
-	value: 'Xpd8XqTYX0mYXpPJXC_XTiqboFICD2YUDT8HfGB3',
+	value: 'XpdJXq9YX0XYX5doXC_Spdta1lW~6LOyGxIbGdk1',
 	domain: 'mms.pinduoduo.com',
 }
 
-export const cookies: SetCookie[] = [JSESSIONID, PASS_ID, api_uid1, api_uid2, nano_fp]
+export const cookies: SetCookie[] = [ JSESSIONID,PASS_ID,api_uid1,api_uid2, nano_fp]
